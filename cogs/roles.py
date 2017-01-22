56c85135d601534212c72639073ae0b4cc7bb509
maartyl <kuronomaartyl@gmail.com>
2017-01-23 00:20:14 +0100

    fix: !tag now correctly removes other roles

--- cogs/roles.py
+++ cogs/roles.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord.utils as du
 from functools import partial
 import discord
+import asyncio
 
 def transpose_aliases(aliases):
     # mapping from each alias to their real role name
@@ -41,15 +42,16 @@ class Roles:
         # to match actual roles
         found = du.get(ctx.message.server.roles, name=role.title())
         # currently active tags of the member, except `found` (if present)
-        active = list(filter(lambda x: x != None and x != found,
+        active = list(filter(lambda x: x and x != found,
             (du.get(member.roles, name=name.title())
                 for name in self.roles)))
 
         print(member, ':', found, '<', *active)
 
         # remove active roles -> no more need for !untag
-        #TODO: currently deosn't always delete: why? fix...
         await self.bot.remove_roles(member, *active)
+        #didn't always delete:
+        await asyncio.sleep(1)
         await self.bot.add_roles(member, found)
         return str(found)
 
