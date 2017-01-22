1ff8a19530069d1ee347ea6f1ae748714f4410ac
maartyl <kuronomaartyl@gmail.com>
2017-01-22 16:11:09 +0100

    tiny: remove trailing whitespace

--- cogs/roles.py
+++ cogs/roles.py
@@ -8,7 +8,7 @@ def transpose_aliases(aliases):
     for role, aliases in aliases.items():
         for alias in aliases:
             transposed[alias] = role
-    return transposed 
+    return transposed
 
 class Roles:
 
@@ -17,21 +17,21 @@ class Roles:
         # both must contain lower case only
         self.roles = self.bot.server_info['allowed_roles']
         self.aliases = transpose_aliases(self.bot.server_info['role_aliases'])
- 
+
     @commands.command(pass_context=True,
                       brief='Set your tag to an available role')
     async def tag(self, ctx, role):
         # account for random capitalization in input
         role = role.lower()
-        # resolve alias or use argument as is 
-        role = self.aliases.get(role, role) 
- 
+        # resolve alias or use argument as is
+        role = self.aliases.get(role, role)
+
         if role not in self.roles:
             raise commands.BadArgument(self.role_error())
 
         assigned = await self.modify_roles(ctx, role)
-        await self.bot.reply('You are now tagged as: ' + assigned)  
- 
+        await self.bot.reply('You are now tagged as: ' + assigned)
+
     async def modify_roles(self, ctx, role):
         member = ctx.message.author
 
@@ -39,8 +39,8 @@ class Roles:
         # to match actual roles
         found = du.get(ctx.message.server.roles, name=role.title())
         # currently active tags of the member, except `found` (if present)
-        active = list(filter(lambda x: x != None and x != found, 
-            (du.get(member.roles, name=name.title()) 
+        active = list(filter(lambda x: x != None and x != found,
+            (du.get(member.roles, name=name.title())
                 for name in self.roles)))
 
         print(member, ':', found, '<', *active)
@@ -50,7 +50,7 @@ class Roles:
         await self.bot.remove_roles(member, *active)
         await self.bot.add_roles(member, found)
         return str(found)
- 
+
     def role_error(self):
         roles = map(lambda r: r.title(), self.roles)
         error = 'Allowed roles are: [ {} ]'.format(', '.join(roles))
